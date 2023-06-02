50366d7b14c368f3c98866659f0bf6f8f2408166
Logan <pix.59700@gmail.com>
2023-06-02 14:19:17 +0200

    Add first struct & CameraManager 🚧

---
+++ Assets/_Core/Scripts/CameraManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider)), RequireComponent(typeof(Rigidbody))]
+public class CameraManager : MonoBehaviour
+{
+   public Transform playerCamera;
+   public Transform AfterCameraPosition;
+   public float Speed;
+
+   private void Start()
+   {
+      playerCamera = GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+   }
+
+   private void OnTriggerStay(Collider _collider)
+   {
+      if (_collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+      {
+         if (playerCamera != AfterCameraPosition)
+         {
+            MoveCamera();
+         }
+      }
+   }
+
+   void MoveCamera()
+   {
+      playerCamera.position = Vector3.Lerp(playerCamera.position, AfterCameraPosition.position, Time.deltaTime * Speed);
+      playerCamera.rotation = Quaternion.Lerp(playerCamera.rotation, AfterCameraPosition.rotation, Time.deltaTime * Speed);
+   }
+}
